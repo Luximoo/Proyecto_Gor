14e3297e27f4a4f546b04735b0b7f8b0f28c0df3
David <davidluxi@gmail.com>
2025-03-09 23:27:41 -0600

    se quitaron los chekbox, para agregar un boton que te da las opciones de lo que quieres agregar ejemplo una de chicharron con papas

--- src/views/Rea_Pedido.py
+++ src/views/Rea_Pedido.py
@@ -4,12 +4,43 @@ from Utilidades.utils import *
 
 def View_RealizarPedido(page):
     navegacion = crear_navegacion(page)
+    son = 0
+    def handle_dismissal(e):
+        page.add(ft.Text("Bottom sheet dismissed"))
+    bs2 = ft.Column(
+            tight=True,
+            controls=[
+                ft.ElevatedButton("Close bottom sheet", on_click=lambda _: page.close(bs)),
+            ],
+            scroll=ft.ScrollMode.ALWAYS
+        )
+    bs1= ft.Container(
+            padding=50,
+            content=bs2
+        )
+    bs = ft.BottomSheet(
+        on_dismiss=handle_dismissal,
+        content=bs1,
+    )
+    
+    def conmas(index):
+        def handler(e):
+            global son
+            print(f"de: {guisosT[son]} con: {guisosT[index]}")
+            page.update()
+        return handler
+
+    
     gorLista = ft.ExpansionTile(
             title=ft.Text("Gorditas"),
             leading=ft.Icon(ft.icons.INFO_OUTLINE, color="#4A90E2"),
             icon_color="#4A90E2",
             shape=ft.RoundedRectangleBorder(radius=12),
             )
+    def holl(e):
+        global son
+        son = e.control.key
+        page.open(bs)
     def incrementar(index):
         def handler(e):
             valores[index] += 1  # Incrementar el valor
@@ -30,8 +61,11 @@ def View_RealizarPedido(page):
     valores = [0,0,0,0,0,0,0,0]
     botones_suma = []
     botones_resta = []
+    botones_complemento = []
     columna = ft.Column(controls=[],scroll=ft.ScrollMode.ALWAYS)
     for va in range (len(guisosT)):
+        botones_complemento.append(ft.ElevatedButton(data=[va],text=f"Con: {guisosT[va]}",on_click=conmas(va)))
+        bs2.controls.append(botones_complemento[va])
         textoG.append(ft.Container(
                 content=ft.Text(f"{guisosT[va]}",size=16,  # Tamaño del texto
                     weight=ft.FontWeight.BOLD,  # Negrita
@@ -71,10 +105,11 @@ def View_RealizarPedido(page):
         ]))
         columna.controls.append(guisosF[va])
         #gorLista.controls.append(columna.controls[va])
-        columna.controls.append(ft.Column(controls=[
+        """columna.controls.append(ft.Column(controls=[
                 ft.Checkbox(label="Con queso", value=False),
                 ft.Checkbox(label="Con Frijoles", value=False),
-        ],spacing=0) )
+        ],spacing=0) )"""
+        columna.controls.append(ft.ElevatedButton(key=va,text="Opciones", on_click=holl))
         print(columna.controls[va])
     for va in range(len(guisosT)*2):
         gorLista.controls.append(columna.controls[va])
